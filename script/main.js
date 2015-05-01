dbd17afe683044d7fdcb9d5ce54a036348bf19d6
chaniemobley <chaniemobley@gmail.com>
2015-04-30 23:03:24 -0400

    simplified contact form

--- script/main.js
+++ script/main.js
@@ -12,12 +12,5 @@ $(document).ready(function() {
     });
 
 });
-function expandIt(){
-    var expanderTrigger = document.getElementById("js-expander-trigger");
-    var expanderContent = document.getElementById("js-expander-content");
 
-    $('#js-expander-trigger').click(function(){
-        $(this).toggleClass("expander-hidden");
-    });
-}
 
